f0dc7de4c7d0cbe159df53199c163e10d109b45d
Shoji <shojiurid@gmail.com>
2023-04-11 16:11:28 -0600

    feature training-sheet

---
+++ src/pages/admin/capacitaciones/[id].js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const TrainingSheet = props => {
+  return (
+    <div>TrainingSheet</div>
+  )
+}
+
+TrainingSheet.propTypes = {}
+
+export default TrainingSheet
