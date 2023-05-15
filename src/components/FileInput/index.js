6767ae58a395fd7788fa4f6e1565daba5ddb70c0
shoji <shojiurid@gmail.com>
2023-05-15 00:04:42 -0600

    feature file upload

--- src/components/FileInput/index.js
+++ src/components/FileInput/index.js
@@ -1,15 +1,46 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import CustomInput from '../CustomInput'
+import React from "react";
+import PropTypes from "prop-types";
+import CustomInput from "../CustomInput";
+import fetcher from "@/services/fetcher";
 
-const FileInput = props => {
+const FileInput = (props) => {
+  const inputRef = React.useRef(null)
+  const [value, setValue] = React.useState();
+
+  const handleChange = async (inputEvent) => {
+    const { target = {} } = inputEvent;
+    const value = target && target.files[0];
+    const data = new FormData();
+    data.append("file", value);
+
+    const { data: fileUploaded } = await fetcher({
+      url: `/image/upload-file`,
+      method: "POST",
+      data,
+    });
+    if (inputRef.current) inputRef.current.value = '';
+    if (fileUploaded)
+      setValue(`${fetcher.defaults.baseURL}${fileUploaded}`.replace(new RegExp('//', 'g'),'/'));
+  };
   return (
-    <CustomInput label='Foto de Perfil' type='file'>
+    <>
+      <CustomInput label="Foto de Perfil" type="file" onChange={handleChange} />
+      {value && (
+        <img
+          src={value}
+          style={{
+            maxHeight: "100px",
+            maxWidth: "100px",
+            objectFit: "contain",
+            objectPosition: "center",
+          }}
+          alt="preview"
+        />
+      )}
+    </>
+  );
+};
 
-    </CustomInput>
-  )
-}
+FileInput.propTypes = {};
 
-FileInput.propTypes = {}
-
-export default FileInput
+export default FileInput;
